5a390f6846082d7c22e9c9e75815af665f78b480
darcy <acednes@gmail.com>
2024-09-12 10:59:45 +1000

    fix(tests): handle broken asm file

--- tests/assemble.sh
+++ tests/assemble.sh
@@ -10,9 +10,14 @@ for asm in "$examples"/*.asm; do
     filename="$(basename "${asm%%.asm}")"
     obj_actual="$examples/$filename.a.obj"
 
-    laser -a "$asm" >/dev/null || break
-
     printf 'ASSEMBLE    %-18s' "$filename"
+
+    laser -a "$asm" >/dev/null
+    if [ "$?" -ne 0 ]; then
+        echo 'Bad assembly file. Skipping.'
+        continue
+    fi
+
     lasim -a "$asm" -o "$obj_actual"
     if [ "$?" -eq 0 ];
         then echo 'pass'
@@ -26,6 +31,11 @@ for obj_actual in "$examples"/*.a.obj; do
     obj_expected="$examples/$filename.obj"
 
     printf 'CHECK       %-18s' "$filename"
+
+    if [ ! -f "$obj_expected" ]; then
+        echo 'No object file. Skipping.'
+        continue
+    fi
 
     diff "$obj_expected" "$obj_actual" >/dev/null
     if [ "$?" -eq 0 ];
