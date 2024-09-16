2b44cec11a1c45fc412cb89a800bdada7da0b7a7
darcy <acednes@gmail.com>
2024-09-16 22:15:53 +1000

    refactor(tests/assemble): sandbox compiled examples for assemble.sh

--- tests/assemble.sh
+++ tests/assemble.sh
@@ -2,14 +2,24 @@
 
 source "$(dirname $0)/shared.sh"
 
+[ -d "$out/examples" ] || mkdir "$out/examples"
+rm -f "$out/examples/"*
+for asm in "$examples/"*.asm; do
+    cp "$asm" -t "$out/examples"
+done
+
 echo '------'
-for asm in "$examples"/*.asm; do
+for asm in "$out/examples"/*.asm; do
     filename="$(basename "${asm%%.asm}")"
-    obj_actual="$examples/$filename.a.obj"
+    obj_actual="$out/examples/$filename.actual.obj"
+    obj_default="$out/examples/$filename.obj"
+    obj_expected="$out/examples/$filename.expected.obj"
 
     printf 'ASSEMBLE    %-18s' "$filename"
 
     lc3as "$asm" >/dev/null
+    mv -T "$obj_default" "$obj_expected"
+
     if [ $? -ne 0 ]; then
         echo 'Bad assembly file. Skipping.'
         continue
@@ -20,9 +30,9 @@ for asm in "$examples"/*.asm; do
 done
 
 echo '------'
-for obj_actual in "$examples"/*.a.obj; do
-    filename="$(basename "${obj_actual%%.a.obj}")"
-    obj_expected="$examples/$filename.obj"
+for obj_actual in "$out/examples"/*.actual.obj; do
+    filename="$(basename "${obj_actual%%.actual.obj}")"
+    obj_expected="$out/examples/$filename.expected.obj"
 
     printf 'CHECK       %-18s' "$filename"
 
